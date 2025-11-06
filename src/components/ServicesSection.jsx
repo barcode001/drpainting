545fbcbfdf66e587ad3420e9f1579c261f863744
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-11-05 20:12:35 -0600

    changed secondary color

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,40 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import useRevealOnScroll from "./useRevealOnScroll";
-// import { servicesByLocation } from "../config/servicesData";
-
-// export default function ServicesSection({ locationKey }) {
-//   useRevealOnScroll();
-//   const services = servicesByLocation[locationKey] || [];
-
-//   return (
-//     <section className="services" id="services">
-//       <div className="container">
-//         <h2 className="section-title reveal">
-//           Our Painting Services in {locationKey.replace(/([A-Z])/g, " $1")}
-//         </h2>
-//         <div className="services-grid">
-//           {services.map((service, index) => (
-//             <div className="service-card" key={index}>
-//               <img src={service.image} alt={service.alt} loading="lazy" />
-//               <h3 className="reveal">{service.title}</h3>
-//               <p className="reveal">{service.description}</p>
-//               <div className="card-btn reveal">
-//                 <Link to={service.path} className="hero-button secondary">
-//                   <span className="visually-hidden">
-//                     Learn more about {service.title} in {locationKey}
-//                   </span>
-//                   <span aria-hidden="true">Learn More</span>
-//                 </Link>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useRevealOnScroll from "./useRevealOnScroll";
@@ -43,7 +6,6 @@ import { PiHandTapLight } from "react-icons/pi";
 
 export default function ServicesSection({ locationKey }) {
   useRevealOnScroll();
-  // const [flippedIndex, setFlippedIndex] = useState(null);
 
   const services = servicesByLocation[locationKey] || [];
   // ✅ Track multiple flipped cards using object
